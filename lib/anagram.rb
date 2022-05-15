e0e582f79ca712442a28c0615455152ff08b6199
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-15 12:40:08 -0700

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.select do |w|
+            w.chars.sort == @word.chars.sort
+            # binding.pry
+        end
+    end
+end
